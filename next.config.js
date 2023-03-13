c020a6fe52d3b72781c3107ffcc5c751258d7ae8
IvanLM1098 <ivanleonmartin10@gmail.com>
2023-03-13 20:23:20 +0100

    img / Image banner, next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+        pathname: '/tzu1pd8bi7co/1Flr57WezrXCKPTdLqeWZB/9dd5b582d206c219c270d110f12e7c78/banner-home-tablet_2x.jpg?fm=webp&fit=scale&w=640&h=548',
+      },
+    ],
+  },
+  images: {
+    domains: ['images.ctfassets.net'],
+  },
 }
 
 module.exports = nextConfig
