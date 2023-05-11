882c2caded410589ef16802ff16676eded196f8f
IvanLM1098 <ivanleonmartin10@gmail.com>
2023-05-11 13:41:38 +0200

    cambios cloudflare

--- components/AdBanner.tsx
+++ components/AdBanner.tsx
@@ -10,7 +10,7 @@ const AdBanner: React.FC = () => {
             if (src.startsWith('https://')) {
               return src;
             } else {
-              return `/images/${src}`;
+              return `${src}`;
             }
           }}
           src="https://images.ctfassets.net/tzu1pd8bi7co/1Flr57WezrXCKPTdLqeWZB/9dd5b582d206c219c270d110f12e7c78/banner-home-tablet_2x.jpg?fm=webp&fit=scale&w=640&h=548" 
