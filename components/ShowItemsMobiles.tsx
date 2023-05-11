3d1143e2dad5828344279875a21ef4c93262cf6d
IvanLM1098 <ivanleonmartin10@gmail.com>
2023-05-11 13:21:04 +0200

    cambios cloudflare

--- components/ShowItemsMobiles.tsx
+++ components/ShowItemsMobiles.tsx
@@ -12,7 +12,15 @@ const ShowItemsMobiles = ({ data }: Props) => (
   <div className={styles.grid}>
     {data.map((item) => (
       <div className={styles.item} key={item.id}>
-        <Image className={styles.image} src={item.image} alt="Imagen móvil" width={170} height={170} priority/>
+        <Image 
+          loader={({ src }) => {
+            if (src.startsWith('https://')) {
+              return src;
+            } else {
+              return `/images/${src}`;
+            }
+          }}
+          className={styles.image} src={item.image} alt="Imagen móvil" width={170} height={170} priority/>
         <h3 className={styles.text}>{item.title}</h3>
         <p className={styles.p}>{item.price}</p>
       </div>
